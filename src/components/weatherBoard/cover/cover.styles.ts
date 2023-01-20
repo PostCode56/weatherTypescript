1138a95221e76fcb0683adeaabcc593f3a548a76
PostCode56 <hipgit@inbox.ru>
2023-01-20 14:16:55 +0300

    test

--- src/components/weatherBoard/cover/cover.styles.ts
+++ src/components/weatherBoard/cover/cover.styles.ts
@@ -1,21 +1,26 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
+  margin: 0 auto;
   text-align: center;
 `
 export const StyledCoverIcon = styled.img`
   width: 600px;
   height: 600px;
-  @media (min-width: 250px) and (max-width: 925px) {
+  @media (min-width: 300px) and (max-width: 925px) {
     width: 350px;
     height: 350px;
   }
+  @media (min-width: 250px) and (max-width: 299px) {
+    width: 280px;
+    height: 280px;
+  }
 `
 export const StyledTitle = styled.div`
-  margin-top: 20px;
+  margin-bottom: 20px;
   font-size: 36px;
   color: white;
   @media (min-width: 250px) and (max-width: 399px) {
-    font-size: 22px;
+    font-size: 24px;
   }
 `
